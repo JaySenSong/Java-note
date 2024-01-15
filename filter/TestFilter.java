c91145835411d25fa4d72e50babee4e9ab7d2a5a
JaySenSong <155499008+JaySenSong@users.noreply.github.com>
2024-01-15 10:42:20 +0800

    Update TestFilter.java

--- filter/TestFilter.java
+++ filter/TestFilter.java
@@ -2,6 +2,13 @@
 @Component
 @Order(1)
 public class TestFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+    
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
@@ -14,4 +21,10 @@ public class TestFilter implements Filter {
       
         filterChain.doFilter(requestWrapper, servletResponse);
     }
+
+    @Override
+    public void destroy() {
+        Filter.super.destory();
+    }
+    
 }
