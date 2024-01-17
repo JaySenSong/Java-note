f9d61115cd51edaff823e0dbd64c27bcbcd4a2d6
JaySenSong <155499008+JaySenSong@users.noreply.github.com>
2024-01-17 16:53:59 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,32 @@
+//test service
+@SpringbootTest
+
+//test controller
+@WebMvcTest(value = User.class, exculdAutoConfiguration = SecurityAutoConfiguration.class,exludeFilters={@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = xxx.class)})
+@AutoConfigureMockMvc
+
+
+//test repo
+@DataJpaTest
+
+
+
+  ==========================
+
+@Captor
+ArgumentCaptor<Email> emailCaptor;
+
+@Test
+void whenDoesSupportHtml_expectHTMLEmailFormat() {
+    String to = "info@baeldung.com";
+    String subject = "Using ArgumentCaptor";
+    String body = "Hey, let'use ArgumentCaptor";
+
+    emailService.send(to, subject, body, true);
+
+  
+    // ArgumentCaptor<Email> emailCaptor = ArgumentCaptor.forClass(Email.class);   //同@Captor
+    verify(platform).deliver(emailCaptor.capture());
+    Email value = emailCaptor.getValue();
+    assertThat(value.getFormat()).isEqualTo(Format.HTML);
+}
