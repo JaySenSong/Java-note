00f1dc9db48d75a5f8e2f0fafb61d6ac25161140
JaySenSong <155499008+JaySenSong@users.noreply.github.com>
2024-10-11 09:55:20 +0800

    Update Tutorial.java

--- Tutorial.java
+++ Tutorial.java
@@ -9,15 +9,26 @@
 public class Tutorial{
 
   public void main(){
+    
     User resUser = inputSupplier(()->{
       User user = new User();
       user.setName("Jay");
       return user;
     });
+
+    List<A> distinctA = aList.stream().filter(distinctByInput(a::getId)).collect(Collectors.toList());
+    
   }
 
   public <T> T inputSupplier(Supplier<T> input){
     return input.get();
+  }
+
+  // Predicate返回給Filter使用
+  public <T> Predicate<T> distinctByInput(Function<? super T, Object> func){
+    // 安全線程 ConcurrentHashMap.newKeySet();
+    Set<Object> set = new HashSet<>();
+    return t -> set add(func.apply(t))
   }
 
   
